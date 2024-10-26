d4718211b58102ba0e69c22eef76aa35bea6405f
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-10-26 20:40:52 +0200

    first commit

---
+++ US States Games/main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+screen=Screen()
+
+screen.setup(width=725,height=491)
+screen.bgpic('blank_states_img.gif')
+screen.title('Name the States')
+user_input=screen.textinput("hej","chuj:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
