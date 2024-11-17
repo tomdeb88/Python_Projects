983d4cea33faac568707efb10c8de98eec6a81f9
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-11-17 20:52:21 +0100

    add generating french words

--- Flash Card App (tkinter)/main.py
+++ Flash Card App (tkinter)/main.py
@@ -1,6 +1,26 @@
+from random import random
 from tkinter import *
-
+import pandas as pd
+import random
 BACKGROUND_COLOR = "#B1DDC6"
+
+df=pd.read_csv('./data/french_words.csv')
+words_list=df.to_dict(orient='records')
+
+
+def display_new_word():
+    random_pair = random.choice(words_list)
+    canvas.itemconfig(bottom_text, text=random_pair['French'])
+    canvas.itemconfig(top_text, text='French')
+
+
+
+
+
+
+
+
+
 
 window=Tk()
 window.title('Flashy')
@@ -14,34 +34,20 @@ canvas.grid(column=0,row=0,columnspan=2)
 
 #Texts
 top_text=canvas.create_text(400,150,text="Title",font=('Ariel',40,'italic'))
-bottom_text=canvas.create_text(400,263,text="Word",font=('Ariel',60,'bold'))
-
-
+bottom_text=canvas.create_text(400,263,text="word",font=('Ariel',60,'bold'))
 
 #Buttons
 x_button_image=PhotoImage(file='./images/wrong.png')
-x_button=Button(image=x_button_image,highlightthickness=0,borderwidth=0)
+x_button=Button(image=x_button_image,highlightthickness=0,borderwidth=0,command=display_new_word)
 x_button.grid(column=0,row=1)
 
 y_button_image=PhotoImage(file='./images/right.png')
-y_button=Button(image=y_button_image,highlightthickness=0,borderwidth=0)
+y_button=Button(image=y_button_image,highlightthickness=0,borderwidth=0,command=display_new_word)
 y_button.grid(column=1,row=1)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+display_new_word()
 window.mainloop()
 
