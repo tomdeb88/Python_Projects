a3bfdef801f6ba0d2a5d1dfc2316c5adc9d3f527
Tomasz Debowski <tomaszdebowski88@gmail.com>
2024-11-17 23:22:41 +0100

    add flipping cards

--- Flash Card App (tkinter)/main.py
+++ Flash Card App (tkinter)/main.py
@@ -2,34 +2,46 @@ from random import random
 from tkinter import *
 import pandas as pd
 import random
+
+
 BACKGROUND_COLOR = "#B1DDC6"
 
 df=pd.read_csv('./data/french_words.csv')
 words_list=df.to_dict(orient='records')
 
+english_word=""
 
 def display_new_word():
+    global english_word, timer
+    window.after_cancel(timer)
     random_pair = random.choice(words_list)
-    canvas.itemconfig(bottom_text, text=random_pair['French'])
-    canvas.itemconfig(top_text, text='French')
+    canvas.itemconfig(bottom_text, text=random_pair['French'],fill='black')
+    canvas.itemconfig(top_text, text='French',fill='black')
+    english_word=random_pair['English']
+    canvas.itemconfig(background,image=front_card)
+    timer=window.after(3000,eng_card)
 
 
 
 
 
 
-
-
+def eng_card():
+    canvas.itemconfig(background,image=back_card)
+    canvas.itemconfig(bottom_text,fill='white', text=english_word)
+    canvas.itemconfig(top_text,fill='white', text='English')
 
 
 window=Tk()
 window.title('Flashy')
 window.config(bg=BACKGROUND_COLOR,padx=50,pady=50)
+timer=window.after(3000,eng_card)
 
 #Canvas
 canvas=Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
 front_card=PhotoImage(file='./images/card_front.png')
-canvas.create_image(400,263,image=front_card)
+back_card=PhotoImage(file='./images/card_back.png')
+background=canvas.create_image(400,263,image=front_card)
 canvas.grid(column=0,row=0,columnspan=2)
 
 #Texts
@@ -44,7 +56,6 @@ x_button.grid(column=0,row=1)
 y_button_image=PhotoImage(file='./images/right.png')
 y_button=Button(image=y_button_image,highlightthickness=0,borderwidth=0,command=display_new_word)
 y_button.grid(column=1,row=1)
-
 
 
 
